6a170ad9d239e71c351dc52d9522aa4d1ea6e1bd
cleverman88 <sohaibkarous@gmail.com>
2020-02-09 04:37:28 +0000

    Added the database

--- barcode_information_extractor.py
+++ barcode_information_extractor.py
@@ -5,6 +5,15 @@ from PIL import Image
 import requests
 from bs4 import BeautifulSoup as bs
 
+
+product_database = {
+    "5000118047817": 'Lipton Peach Ice Tea 500ml',
+    "90162602": 'Red Bull Energy Drink 250ml',
+    "5033022002306": 'Rock Spring Water 500ml',
+    "5012035950132": 'Haribo Tangfastics'
+}
+
+
 class CSVWriter():
     def __init__(self, barcode_data):
         pass
@@ -29,6 +38,8 @@ class Product():
         return result.group(1)
     
     def parse_product_name_hardcoded(self):
+        if self.barcode_number in product_database:
+            return product_database[self.barcode_number]
         return "Bruh Cheese"
 
 
